e601af71008c33867d51c9e5feb2dca958496335
anishsharma21 <anisharma2103@gmail.com>
2024-10-01 18:31:58 +1000

    Overwrote the initial .scm program, just wrote a simple program for doing basic arithmetic in Scheme, will work my way up from here

--- 1.scm
+++
@@ -1,6 +0,0 @@
-(define (factorial n))
-    (if (= n 0)
-        1
-        (* n (factorial (n-1)))))
-
-(display (factorial n))
---
+++ arithmetic.scm
@@ -0,0 +1,9 @@
+#lang racket
+
+(+ 3 5)
+(- 7 9)
+(* 8 3)
+
+(define n 5)
+(display n)
+(newline)
