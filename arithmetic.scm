63698a4591b58d14b84b260ca9f029237cf74d3a
anishsharma21 <anisharma2103@gmail.com>
2024-10-03 02:06:16 +1000

    simple update to arithmetic file

--- arithmetic.scm
+++ arithmetic.scm
@@ -7,3 +7,13 @@
 (define n 5)
 (display n)
 (newline)
+
+(* n n)
+(* (- n 1) n)
+
+(define pi 3.147)
+(define radius 3)
+(define area (* pi radius radius ))
+(define circumference ( * 2 pi radius ))
+area
+circumference
